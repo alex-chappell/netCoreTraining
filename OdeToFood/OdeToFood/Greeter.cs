95d443cc28d51d080eeb80c720961ec0a70ae603
Alexandra Chappell <achappell@maximtrak.com>
2018-08-30 14:16:34 -0400

    Project Setup

---
+++ OdeToFood/OdeToFood/Greeter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public class Greeter : IGreeter
+    {
+        private readonly IConfiguration _configuration;
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetMessage()
+        {
+            return "Greetings!";
+        }
+    }
+}
